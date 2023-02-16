c0e1138cd92d397d1aa5f9410cd33811fe402e28
Neel-Wappnet <neel.wappnet@gmail.com>
2023-02-16 13:50:20 +0530

    databse reset
    using new orm

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import { config } from "dotenv"
 import express,{ Express, Request, Response, } from "express"
-import sequelizeConnection from "./config/dbConnect"
 import loginRoute from "./routers/login.route"
 
 const app: Express = express()
@@ -14,9 +13,7 @@ app.get("/", (req: Request, res: Response) => {
   res.json({msg:"Hello World!"})
 })
 
-sequelizeConnection.authenticate()
-.then(()=>console.log("database connected...!!"))
-.catch((err)=>console.log(err.massage))
+
 
 const PORT = process.env.PORT
 app.listen(PORT,()=>{
